4de3c912879c50d26a1e301fdc7b376d6cefe8a8
Kkenize <Kkenizez@gmail.com>
2025-01-13 18:36:31 -0500

    Initial Commit

---
+++ BoogieWonderland/BoogieWonderlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoogieWonderlandApp.swift
+//  BoogieWonderland
+//
+//  Created by Zhejun Zhang on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BoogieWonderlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
